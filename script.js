01932a114dcbb1ed78f40207f228f59d399bcddb
Jihyun-Jang <jhjang742@gmail.com>
2020-06-28 00:46:04 +0100

    Added JavaScript file

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberBtns = document.querySelectorAll('.number');
+const operationBtns = document.querySelectorAll('.operator');
+const equalsBtn = document.querySelector('#equals');
+const clearBtn = document.querySelector('#clear');
+const allClearBtn = document.querySelector('#all-clear');
+const previousTextBtn = document.querySelector('#previous-text');
+const currentTextBtn = document.querySelector('#current-text');
