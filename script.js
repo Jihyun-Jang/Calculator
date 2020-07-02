f7d62a3947a48a60313c276c840c259b53031f99
Jihyun-Jang <jhjang742@gmail.com>
2020-07-03 00:35:49 +0100

    Added clear function and fixed many bugs

--- script.js
+++ script.js
@@ -2,47 +2,67 @@ const buttons = document.querySelectorAll('button');
 const curDisplay = document.querySelector('#current-display');
 const clacDisplay = document.querySelector('#calculation-display');
 
-buttons.forEach(el => el.addEventListener('click', calculation));
+buttons.forEach(el => el.addEventListener('click', calculate));
 
 let calculatingArr = [];
 let result;
+let isCalculating = false;
 
 
-function calculation() {
+function calculate() {
 
    if(this.classList[0] === 'number') {
+      if(isCalculating === false) {
+         curDisplay.innerText = '';
+         clacDisplay.innerText = '';
+         calculatingArr = [];
+         isCalculating = true;
+      }
+
+      if(this.id === 'decimal' && curDisplay.innerText.includes('.')) return;
+
       curDisplay.innerText += this.innerText;
       
    }
 
 
    if(this.classList[0] === 'operator') {
-      if(curDisplay.innerText === '') return
-      else {
-         calculatingArr.push(curDisplay.innerText);
-         calculatingArr.push(this.value);
-         clacDisplay.innerText += curDisplay.innerText + this.innerText;
-         curDisplay.innerText = '';
-         
+      if(curDisplay.innerText === '') return;
+      else if(isCalculating === false) {
+         calculatingArr = [];
+         clacDisplay.innerText = '';
+         isCalculating = true;       
       }
+      
+      calculatingArr.push(curDisplay.innerText);
+      calculatingArr.push(this.value);
+      clacDisplay.innerText += Number(curDisplay.innerText) + this.innerText;
+      curDisplay.innerText = '';        
+      
    }
 
 
    if(this.id === 'all-clear') {
       curDisplay.innerText = '';
       clacDisplay.innerText = '';
+      calculatingArr = [];
    }
+
 
    if(this.id === 'clear') {
-      console.log(this.innerText);
+      if(isCalculating === false) return;
+      curDisplay.innerText = curDisplay.innerText.slice(0, -1);
    }
 
+
    if(this.id === 'equals') {
+      if(isCalculating === false) return;
+      if(curDisplay.innerText === '') return;
       calculatingArr.push(curDisplay.innerText);
-      clacDisplay.innerText += curDisplay.innerText;
+      clacDisplay.innerText += Number(curDisplay.innerText);
       result = eval(calculatingArr.join(' '));
-      curDisplay.innerText = result;
-      calculatingArr = [];
-   }   
+      curDisplay.innerText = result;      
+      isCalculating = false;
+   }
    
 }
