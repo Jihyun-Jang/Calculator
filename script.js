e915cca99fa6f0bfcddc5d2d6a3aa37362f3fb04
Jihyun-Jang <jhjang742@gmail.com>
2020-07-02 21:06:27 +0100

    Added basic functions for calculating

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-const numberBtns = document.querySelectorAll('.number');
-const operationBtns = document.querySelectorAll('.operator');
-const equalsBtn = document.querySelector('#equals');
-const clearBtn = document.querySelector('#clear');
-const allClearBtn = document.querySelector('#all-clear');
-const previousTextBtn = document.querySelector('#previous-text');
-const currentTextBtn = document.querySelector('#current-text');
+const buttons = document.querySelectorAll('button');
+const curDisplay = document.querySelector('#current-display');
+const clacDisplay = document.querySelector('#calculation-display');
+
+buttons.forEach(el => el.addEventListener('click', calculation));
+
+let calculatingArr = [];
+let result;
+
+
+function calculation() {
+
+   if(this.classList[0] === 'number') {
+      curDisplay.innerText += this.innerText;
+      
+   }
+
+
+   if(this.classList[0] === 'operator') {
+      if(curDisplay.innerText === '') return
+      else {
+         calculatingArr.push(curDisplay.innerText);
+         calculatingArr.push(this.value);
+         clacDisplay.innerText += curDisplay.innerText + this.innerText;
+         curDisplay.innerText = '';
+         
+      }
+   }
+
+
+   if(this.id === 'all-clear') {
+      curDisplay.innerText = '';
+      clacDisplay.innerText = '';
+   }
+
+   if(this.id === 'clear') {
+      console.log(this.innerText);
+   }
+
+   if(this.id === 'equals') {
+      calculatingArr.push(curDisplay.innerText);
+      clacDisplay.innerText += curDisplay.innerText;
+      result = eval(calculatingArr.join(' '));
+      curDisplay.innerText = result;
+      calculatingArr = [];
+   }   
+   
+}
