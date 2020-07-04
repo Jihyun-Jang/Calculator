2cf7db8dfd7306d60bc78349b5f2b2d3a76bfd5e
Jihyun-Jang <jhjang742@gmail.com>
2020-07-04 19:12:57 +0100

    Tidied up functions

--- script.js
+++ script.js
@@ -11,113 +11,104 @@ let isCalculating = false;
 
 function calculate() {
 
-   if(this.classList[0] === 'number') {
-      if(isCalculating === false) {
-         curDisplay.innerText = '';
-         clacDisplay.innerText = '';
-         calculatingArr = [];
-         isCalculating = true;
-      }
+   if(this.classList[0] === 'number') clickNumber(event);
 
-      if(this.id === 'decimal' && curDisplay.innerText.includes('.')) return;
+   if(this.classList[0] === 'operator') clickOperator(event);
 
-      curDisplay.innerText += this.innerText;
-      
-   }
+   if(this.id === 'all-clear') clickAllClear();
+
+   if(this.id === 'clear') clickClear();
+
+   if(this.id === 'equals') clickEquals();
+   
+}
 
 
-   if(this.classList[0] === 'operator') {
-      if(isCalculating === false) {
-         if(this.id === 'subtract') {
-            if(calculatingArr.length !== 0) {
-               calculatingArr = [];
-               clacDisplay.innerText = '';
-               isCalculating = true;
-               calculatingArr.push(curDisplay.innerText);
-               calculatingArr.push(this.value);
-               clacDisplay.innerText += ' ' + Number(curDisplay.innerText) + ' ' + this.innerText;
-               curDisplay.innerText = '';
-            } else { // 맨첨 시작
-               curDisplay.innerText = this.innerText;
-               isCalculating = true;
-            }
-
-         } else { //isCalculating === false && this.id !== 'subtract'
-            if(curDisplay.innerText === '') return;
-            else {
-               calculatingArr = [];
-               clacDisplay.innerText = '';
-               isCalculating = true;
-               calculatingArr.push(curDisplay.innerText);
-               calculatingArr.push(this.value);
-               clacDisplay.innerText += ' ' + Number(curDisplay.innerText) + ' ' + this.innerText;
-               curDisplay.innerText = '';
-            }
-         }
-
-      } else { // isCalculating === true
-         if(this.id === 'subtract') {
-            if(curDisplay.innerText === '-') return;
-            else if(curDisplay.innerText === '') {
-               curDisplay.innerText = this.innerText;
-            } else {
-               calculatingArr.push(curDisplay.innerText);
-               calculatingArr.push(this.value);
-               clacDisplay.innerText += ' ' + Number(curDisplay.innerText) + ' ' + this.innerText;
-               curDisplay.innerText = '';
-            }
-
-         } else { // true고 && subtract 아님
-            if(curDisplay.innerText === '-') return;
-            else if(curDisplay.innerText === '') return;
-            else {
-               calculatingArr.push(curDisplay.innerText);
-               calculatingArr.push(this.value);
-               clacDisplay.innerText += ' ' + Number(curDisplay.innerText) + ' ' + this.innerText;
-               curDisplay.innerText = '';
-            }
-         }
-      }
-
-
-
-
-      
-      // if(curDisplay.innerText === '') return;
-      // else if(isCalculating === false) {
-      //    calculatingArr = [];
-      //    clacDisplay.innerText = '';
-      //    isCalculating = true;       
-      // }
-      
-      // calculatingArr.push(curDisplay.innerText);
-      // calculatingArr.push(this.value);
-      // clacDisplay.innerText += Number(curDisplay.innerText) + this.innerText;
-      // curDisplay.innerText = '';        
-   }
-
-
-   if(this.id === 'all-clear') {
+function clickNumber(event) {
+   if(isCalculating === false) {
       curDisplay.innerText = '';
       clacDisplay.innerText = '';
       calculatingArr = [];
+      isCalculating = true;
    }
 
+   if(event.target.id === 'decimal' && curDisplay.innerText.includes('.')) return;
 
-   if(this.id === 'clear') {
-      if(isCalculating === false) return;
-      curDisplay.innerText = curDisplay.innerText.slice(0, -1);
-   }
-
-
-   if(this.id === 'equals') {
-      if(isCalculating === false) return;
-      if(curDisplay.innerText === '' || curDisplay.innerText === '-') return;
-      calculatingArr.push(curDisplay.innerText);
-      clacDisplay.innerText += ' ' + Number(curDisplay.innerText);
-      result = eval(calculatingArr.join(' '));
-      curDisplay.innerText = result;      
-      isCalculating = false;
-   }
-   
+   curDisplay.innerText += event.target.innerText;
 }
+
+
+function clickOperator() {
+   if(isCalculating === false) {
+      if(event.target.id === 'subtract') {
+         if(calculatingArr.length !== 0) {
+            empty();
+            update(event);
+
+         } else { // isCalculating === false && this.id === 'subtract' && calculatingArr === []
+            curDisplay.innerText = event.target.innerText;
+            isCalculating = true;
+         }
+
+      } else { // isCalculating === false && this.id !== 'subtract'
+         if(curDisplay.innerText === '') return;
+         else {
+            empty();
+            update(event);
+         }
+      }
+
+   } else { // isCalculating === true
+      if(event.target.id === 'subtract') {
+         if(curDisplay.innerText === '-') return;
+         else if(curDisplay.innerText === '') {
+            curDisplay.innerText = event.target.innerText;
+         } else update(event);         
+
+      } else { // isCalculating === true && this.id !== 'subtract'
+         if(curDisplay.innerText === '-') return;
+         else if(curDisplay.innerText === '') return;
+         else update(event);         
+      }
+   } 
+}
+
+
+function clickAllClear() {
+   curDisplay.innerText = '';
+   clacDisplay.innerText = '';
+   calculatingArr = [];
+   isCalculating = false;
+}
+
+
+function clickClear() {
+   if(isCalculating === false) return;
+   curDisplay.innerText = curDisplay.innerText.slice(0, -1);
+}
+
+
+function clickEquals() {
+   if(isCalculating === false) return;
+   if(curDisplay.innerText === ''|| curDisplay.innerText === '-') return;
+   calculatingArr.push(curDisplay.innerText);
+   clacDisplay.innerText += ' ' + Number(curDisplay.innerText);
+   result = eval(calculatingArr.join(' '));
+   curDisplay.innerText = result;      
+   isCalculating = false;
+}
+
+
+function empty() {
+   calculatingArr = [];
+   clacDisplay.innerText = '';
+   isCalculating = true;
+}
+
+
+function update(event) {
+   calculatingArr.push(curDisplay.innerText);
+   calculatingArr.push(event.target.value);
+   clacDisplay.innerText += ' ' + Number(curDisplay.innerText) + ' ' + event.target.innerText;
+   curDisplay.innerText = '';
+} 
